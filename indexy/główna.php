1bfcd6b0aea93017033c37f157b76bdb7663a6dc
szorcyk <szorcantoni@gmail.com>
2025-03-12 16:25:29 +0200

    Add files via upload

---
+++ indexy/główna.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serwis Samochodowy</title>
+    <link rel="stylesheet" href="glowna.css">
+</head>
+<body>
+    <section class="reklama"></section>
+    <header>
+        <h1>Mega serwis</h1>
+        <p>Mały opis</p>
+    </header>
+    <section class="main">
+        <p>Jakiś większy bloczek tekstu</p>
+    </section>
+    <footer></footer>
+</body>
+</html>
