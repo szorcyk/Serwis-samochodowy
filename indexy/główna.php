3cdeb3f9cee4a42dbd1694935b164a0f76f678d3
Hrysevich Viktoryia <Vik.hrysevich.work@gmail.com>
2025-03-14 22:08:51 +0100

    Update główna.php

--- indexy/główna.php
+++ indexy/główna.php
@@ -8,12 +8,37 @@
 </head>
 <body>
     <header>
-        <h1>Mega serwis</h1>
-        <p>Mały opis</p>
+    <div class="header">
+    <a href="#default">TURBOFIX</a>
+        <div class="header-right">
+          <a class="active" href="#home">ZALOGUJ</a>
+          <a href="#services">USŁUGI</a>
+          <a href="#contact">KONTAKT</a>
+          <a href="#price">CENNIK</a>
+        </div>
+      </div> 
+      <div class="header2">
+<!-- TUTAJ TRZEBA TO ZDJECIE NA BANER-->
+        <h1>TURBOFIX AUTO SERWIS</h1>
+        <h3>TWOJE AUTO,NASZA PASJA!</h3>
+      </div>   
     </header>
     <section class="main">
-        <p>Jakiś większy bloczek tekstu</p>
+        <p>W TurboFix Auto Serwis dbamy o Twoje auto jak o własne! Oferujemy kompleksowe naprawy
+        mechaniczne, diagnostykę komputerową i serwis klimatyzacji. Niezawodność, fachowość i uczciwość to
+        nasze priorytety. Od przeglądów po skomplikowane naprawy – z nami Twój samochód jest w
+        najlepszych rękach!</p>
     </section>
-    <footer></footer>
+    <section class="slider">
+      <!-- TUTAJ BEDZUIE TEN SLIDER O DOSTEPNYMI PRZEZ NAS OFERTAMI-->
+
+    </section>
+    <section class="kontakt">
+      <h3>MOZESZ ZAPISAC SIE POPRZEZ NASZA STRONE, NUMER TELEFONU ORAZ PZRYJECHAC!</h3>
+      <input type="button" value="ZKONTAKTUJ SIE Z NAMI!">
+    </section>
+    <footer>
+      <p>Autorzy strony: Hrysevich Viktoryia(JS, HTML&CSS), Kachuk Sofya(UX, UI, PHP, baza danych), Rakowski Kacper(UX, PHP, baza danych), Szorc Antoni(HTML&CSS)</p>
+    </footer>
 </body>
 </html>
