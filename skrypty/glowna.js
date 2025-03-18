86af460695a4fa1311e3c0b282e62b9c1535208a
Hrysevich Viktoryia <Vik.hrysevich.work@gmail.com>
2025-03-18 20:45:00 +0100

    Update glowna.js

--- skrypty/glowna.js
+++ skrypty/glowna.js
@@ -1,1 +1,20 @@
+//to jeszcze nie dziala jak cos
+let currentSlide = 0;
 
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+document.querySelector('.next').addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    updateSlider();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    updateSlider();
+});
+
+function updateSlider() {
+    const slider = document.querySelector('.slider');
+    const slideWidth = slides[0].clientWidth + 20; // Szerokość + margines
+    slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
