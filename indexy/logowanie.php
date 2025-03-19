ed7e003c0cbfbcc3ff0dd10752c212de57efa27e
Hrysevich Viktoryia <Vik.hrysevich.work@gmail.com>
2025-03-19 16:53:47 +0200

    Add files via upload

---
+++ indexy/logowanie.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        *{
+            background-color: #4f3727;
+            color: white;
+            text-align: center;
+            #wracanie{
+                text-decoration: none;
+                float: left;
+            }
+            #login{
+                background-color:#f0e9e2;
+                color: black;
+                border-color: #f0e9e2;
+                text-align: left;
+                font-size:35px;
+                text-decoration: none;
+                border-radius: 20px;
+            }
+            #haslo{
+                background-color: #f0e9e2;
+                color: black;
+                border-color: #f0e9e2;
+                text-align: left;
+                font-size:35px;
+                text-decoration: none;
+                border-radius: 20px;
+
+
+            }
+            #submit{
+                background-color: #780606;
+                font-size:25px;
+                text-decoration: none;
+                border-radius: 20px;
+                padding: 15px;
+                padding-left: 170px;
+                padding-right: 170px;
+
+
+                
+               
+                
+            }
+            h1{
+                font-size: 60px;
+            }
+        }
+    </style>
+    <title>Logowanie</title>
+</head>
+<body>
+<p><a id="wracanie" href="">WRÓĆ</a></p>
+    <div class="logowanie">
+        <h1>ZALOGUJ</h1>
+        <input type="text" id="login" name="login"value="LOGIN"><br><br>
+        <input type="text" id="haslo"name="haslo" value="HASŁO"><br><br>
+        <input type="submit" id="submit" value="WYŚLIJ">
+        <p>LUB <a style="color:#780606"href="">ZAREJESTRUJ SIĘ</a></p>
+    </div>
+    
+</body>
+</html>
---
+++ indexy/rejestracja.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        *{
+            background-color: #4f3727;
+            color: white;
+            text-align: center;
+            #wracanie{
+                text-decoration: none;
+                float: left;
+            }
+            #login{
+                background-color:#f0e9e2;
+                color: black;
+                border-color: #f0e9e2;
+                text-align: left;
+                font-size:35px;
+                text-decoration: none;
+                border-radius: 20px;
+            }
+            #haslo{
+                background-color: #f0e9e2;
+                color: black;
+                border-color: #f0e9e2;
+                text-align: left;
+                font-size:35px;
+                text-decoration: none;
+                border-radius: 20px;
+
+
+            }
+            #numer{
+                background-color:#f0e9e2;
+                color: black;
+                border-color: #f0e9e2;
+                text-align: left;
+                font-size:35px;
+                text-decoration: none;
+                border-radius: 20px;
+
+            }
+            #email{
+                background-color:#f0e9e2;
+                color: black;
+                border-color: #f0e9e2;
+                text-align: left;
+                font-size:35px;
+                text-decoration: none;
+                border-radius: 20px;
+
+            }
+            #submit{
+                background-color: #780606;
+                font-size:25px;
+                text-decoration: none;
+                border-radius: 20px;
+                padding: 15px;
+                padding-left: 170px;
+                padding-right: 170px;
+
+
+                
+               
+                
+            }
+            h1{
+                font-size: 60px;
+            }
+        }
+    </style>
+    <title>ZAREJESTROWANIE SIĘ</title>
+</head>
+<body>
+<p><a id="wracanie" href="">WRÓĆ</a></p>
+    <div class="logowanie">
+        <h1>ZAREJESTRUJ SIĘ</h1>
+        <input type="text" id="login" name="login"value="LOGIN*"><br><br>
+        <input type="text" id="haslo"name="haslo" value="HASŁO*"><br><br>
+        <input type="text" id="numer" name="numer" value="NUMER TELEFONU*"><br><br>
+        <input type="text" id="email" name="email" value="EMAIL"><br><br>
+        <input type="submit" id="submit" value="WYŚLIJ">
+        <p>LUB <a style="color:#780606"href="">ZALOGUJ SIĘ</a></p>
+    </div>
+    
+</body>
+</html>
