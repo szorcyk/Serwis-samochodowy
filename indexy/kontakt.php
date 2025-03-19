d605b7527e168272047bbea31c2d31ffd760349b
szorcyk <szorcantoni@gmail.com>
2025-03-19 13:16:16 +0200

    Test
    Sprawdzam czy łączenie VSC z GitHubem działa

--- indexy/kontakt.php
+++ indexy/kontakt.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+    <div class="header">
+    <a href="#default">TURBOFIX</a>
+        <div class="header-right">
+          <a class="active" href="#home">ZALOGUJ</a>
+          <a href="#contact">KONTAKT</a>
+          <a href="#price">CENNIK</a>
+        </div>
+      </div>
+      
+    <section class="bloczek">
+        <h1>NASZE USŁUGI</h1>
+    </section>
+    <section class="tabela1">
+        <h2>KLIMATYZACJA</h2>
+        <table></table>
+    </section>
 </body>
 </html>
