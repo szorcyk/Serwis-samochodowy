9884e211d585ff2e093cb21d64522a488e7b0673
Hrysevich Viktoryia <Vik.hrysevich.work@gmail.com>
2025-03-19 16:52:55 +0200

    Update główna.php

--- indexy/główna.php
+++ indexy/główna.php
@@ -86,6 +86,21 @@
             <p>montaż, demontaż, wyważanie kół, geometria zawieszenia</p>
             <button>SPRAWDŹ</button>
         </div>
+        <div class="slide" style="background-image: url('hamulce.jpg');">
+            <h2>NAPRAWA UKŁADU HAMULCOWEGO</h2>
+            <p>wymiana klocków, tarcz, przewodów hamulcowych i płynu</p>
+            <button id="slider">SPRAWDŹ</button>
+        </div>
+        <div class="slide" style="background-image: url('zawieszenia.png');">
+            <h2>NAPRAWA ZAWIESZENIA I UKŁADU KIEROWNICZEGO</h2>
+            <p>amortyzatory, wahacze, drążki kierownicze</p>
+            <button id="slider">SPRAWDŹ</button>
+        </div>
+        <div class="slide" style="background-image: url('wydechy.jpg');">
+            <h2>SERWIS UKŁADU WYDECHOWEGO</h2>
+            <p>naprawa lub wymiana tłumików, katalizatorów, sond lambda</p>
+            <button id="slider">SPRAWDŹ</button>
+        </div>
     </div>
     <button class="prev">&#60;</button>
     <button class="next">&#62;</button>
@@ -95,8 +110,42 @@
 
     </section>
     <section class="kontakt">
-      <h3>MOZESZ ZAPISAC SIE POPRZEZ NASZA STRONE, NUMER TELEFONU ORAZ PZRYJECHAC!</h3>
-      <input type="button" value="ZKONTAKTUJ SIE Z NAMI!">
+    <div style="
+    position: relative;
+    background-image: url('main2.jpg');
+    background-size: cover;
+    background-position: center;
+    width: 96,5%;
+    height:400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: white;
+    font-size:30px;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+    text-align: center;
+    padding-left: 50px;
+    margin-bottom:0px;
+    
+">
+<!-- Nakładka przezroczysta -->
+<div style="
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height:400px;
+        background-color:rgba(53, 30, 16, 0.5); /* Brązowy kolor z przezroczystością */
+        z-index: 1;
+       
+    "></div>
+
+    <!-- Tekst na obrazku -->
+    <div style="position: relative; z-index: 2;">
+        <h3 style="margin: 0;">MOZESZ ZAPISAC SIE POPRZEZ NASZA STRONE,<br>NUMER TELEFONU ORAZ PZRYJECHAC!</h3>
+        <input type="button"style="font-size:35px  padding:30px"class="kontakt" value="SKONTAKTUJ SIE Z NAMI!">
+    </div>
+     
     </section>
     <footer>
       <p>Autorzy strony: Hrysevich Viktoryia(JS, HTML&CSS), Kachuk Sofya(UX, UI, PHP, baza danych), Rakowski Kacper(UX, PHP, baza danych), Szorc Antoni(HTML&CSS)</p>
