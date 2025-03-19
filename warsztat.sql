c69268f4467588640b14c5c85b624016479591ee
kapiaq <kapi.raq.k@gmail.com>
2025-03-19 13:07:18 +0100

    Add files via upload

--- warsztat.sql
+++ warsztat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 19, 2025 at 12:59 PM
+-- Generation Time: Mar 19, 2025 at 01:06 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -47,6 +47,16 @@ CREATE TABLE `klienci` (
   `nazwisko` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
+--
+-- Dumping data for table `klienci`
+--
+
+INSERT INTO `klienci` (`ID`, `imie`, `nazwisko`) VALUES
+(1, 'Adam', 'Mickiewicz'),
+(2, 'Andrzej', 'Kmicic'),
+(3, 'Jan', 'Kazimierz'),
+(4, 'Janusz', 'Radziwiłł');
+
 -- --------------------------------------------------------
 
 --
@@ -59,6 +69,13 @@ CREATE TABLE `mechanicy` (
   `nazwisko` text NOT NULL,
   `rodzaj_uslug` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `mechanicy`
+--
+
+INSERT INTO `mechanicy` (`ID`, `imie`, `nazwisko`, `rodzaj_uslug`) VALUES
+(1, 'Jan', 'Sobieski', 'Naprawa układów hamulcowych.\r\nNaprawa zawieszenia i układu kierowniczego.\r\nSerwis układu wydechowego.\r\nWymiana i wyważanie opon.');
 
 -- --------------------------------------------------------
 
@@ -79,7 +96,8 @@ CREATE TABLE `uslugi` (
 --
 
 INSERT INTO `uslugi` (`ID`, `nazwa_uslugi`, `rodzaj_uslugi`, `opis_uslugi`, `cena`) VALUES
-(1, 'Wymiana opon', 'Wulkanizacja', 'Wymiana opon z letnich na zimowe lub na odwrót. Opony dostarcza klient.', 149.99);
+(1, 'Wymiana opon', 'Wulkanizacja', 'Wymiana opon z letnich na zimowe lub na odwrót. Opony dostarcza klient.', 149.99),
+(2, 'Wymiana klocków hamulcowych', 'Naprawa układu hamulcowego', 'Wymiana klocków hamulcowych – przednich lub tylnych, poprawa skuteczności hamowania.', 199.99);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -123,19 +141,19 @@ ALTER TABLE `historia_napraw`
 -- AUTO_INCREMENT for table `klienci`
 --
 ALTER TABLE `klienci`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `mechanicy`
 --
 ALTER TABLE `mechanicy`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `uslugi`
 --
 ALTER TABLE `uslugi`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
