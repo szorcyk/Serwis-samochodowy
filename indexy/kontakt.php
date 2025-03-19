6b26514fd99ee0e99494200309afe7ec8861757c
szorcyk <szorcantoni@gmail.com>
2025-03-19 14:25:40 +0200

    Zapis postępu kontaktu
    Zapisuję na wszelki wypadek dotychczasowy postęp (jeśli można to tak nazwać) nad podstroną Kontakt.php i wraz z nim update styli.

--- indexy/kontakt.php
+++ indexy/kontakt.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
@@ -19,9 +20,45 @@
     <section class="bloczek">
         <h1>NASZE USŁUGI</h1>
     </section>
-    <section class="tabela1">
+    <section class="tabela">
         <h2>KLIMATYZACJA</h2>
-        <table></table>
+        <table>
+            <tr>
+                <th>Napełnianie klimatyzacji</th>
+                <td>Uzupełnienie czynnika chłodzącego, sprawdzenie szczelności</td>
+                <th>150-250zł</th>
+            </tr>
+            <tr>
+                <th>Odgrzybianie i dezynfekcja</th>
+                <td>Usuwanie bakterii i pleśni z układu klimatyzacji</td>
+                <th>70-150zł</th>
+            </tr>
+            <tr>
+                <th>Diagnostyka i naprawa</th>
+                <td>Wykrywanie nieszczelności, naprawa kompresora, wymiana osuszacza</td>
+                <th>100-400zł</th>
+            </tr>
+        </table>
+    </section>
+    <section class="tabela">
+        <h2>WYMIANA OLEJU I FILTRÓW</h2>
+        <table>
+            <tr>
+                <th>Wymiana oleju silnikowego</th>
+                <td>Dobór i wymiana oleju oraz filtra oleju</td>
+                <th>100-200zł</th>
+            </tr>
+            <tr>
+                <th>Wymiana filtrów powietrza i kabinowego</th>
+                <td>Poprawa jakości powietrza w kabinie i silniku</td>
+                <th>50-100zł</th>
+            </tr>
+            <tr>
+                <th>Wymiana filtra paliwa</th>
+                <td>Wykrywanie nieszczelności, naprawa kompresora, wymiana osuszacza</td>
+                <th>80-200zł</th>
+            </tr>
+        </table>
     </section>
 </body>
 </html>
