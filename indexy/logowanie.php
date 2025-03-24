e560d036853e6f34d44a399b507bac59a4ea7481
Hrysevich Viktoryia <Vik.hrysevich.work@gmail.com>
2025-03-24 23:04:47 +0100

    Update logowanie.php

--- indexy/logowanie.php
+++ indexy/logowanie.php
@@ -3,43 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Bricolage Grotesque' rel='stylesheet'>
     <style>
         *{
-            background-color: #4f3727;
+            background-color: #2b1d1d;
             color: white;
             text-align: center;
+            font-family: 'Bricolage Grotesque';
             #wracanie{
                 text-decoration: none;
                 float: left;
+                font-size: 20px;
             }
             #login{
                 background-color:#f0e9e2;
                 color: black;
                 border-color: #f0e9e2;
                 text-align: left;
-                font-size:35px;
+                font-size:45px;
                 text-decoration: none;
                 border-radius: 20px;
+                padding: 5px;
+
             }
             #haslo{
                 background-color: #f0e9e2;
                 color: black;
                 border-color: #f0e9e2;
                 text-align: left;
-                font-size:35px;
+                font-size:45px;
                 text-decoration: none;
                 border-radius: 20px;
+                padding: 5px;
 
 
             }
             #submit{
-                background-color: #780606;
+                background-color: #940000;
                 font-size:25px;
                 text-decoration: none;
                 border-radius: 20px;
                 padding: 15px;
-                padding-left: 170px;
-                padding-right: 170px;
+                padding-left: 210px;
+                padding-right: 210px;
 
 
                 
@@ -47,7 +53,8 @@
                 
             }
             h1{
-                font-size: 60px;
+                font-size: 75px;
+                
             }
         }
     </style>
@@ -55,6 +62,7 @@
 </head>
 <body>
 <p><a id="wracanie" href="">WRÓĆ</a></p>
+<br>
     <div class="logowanie">
         <h1>ZALOGUJ</h1>
         <input type="text" id="login" name="login"value="LOGIN"><br><br>
