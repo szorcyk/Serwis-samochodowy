3acffe537fb63a3e737ea1988ffc8c54e3ebbc03
szorcyk <szorcantoni@gmail.com>
2025-03-24 15:52:42 +0100

    Naming change
    Zmiana nazwy kontakt na usługi co poprawia moją wcześniejszą pomyłkę

---
+++ indexy/uslugi.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+    <div class="header">
+    <a href="#default">TURBOFIX</a>
+        <div class="header-right">
+          <a class="active" href="#home">ZALOGUJ</a>
+          <a href="#contact">KONTAKT</a>
+          <a href="#price">CENNIK</a>
+        </div>
+      </div>
+      
+    <section class="bloczek">
+        <h1>NASZE USŁUGI</h1>
+    </section>
+    <section class="tabela">
+        <h2>KLIMATYZACJA</h2>
+        <table>
+            <tr>
+                <th>Napełnianie klimatyzacji</th>
+                <td>Uzupełnienie czynnika chłodzącego, sprawdzenie szczelności</td>
+                <th>150-250zł</th>
+            </tr>
+            <tr>
+                <th>Odgrzybianie i dezynfekcja</th>
+                <td>Usuwanie bakterii i pleśni z układu klimatyzacji</td>
+                <th>70-150zł</th>
+            </tr>
+            <tr>
+                <th>Diagnostyka i naprawa</th>
+                <td>Wykrywanie nieszczelności, naprawa kompresora, wymiana osuszacza</td>
+                <th>100-400zł</th>
+            </tr>
+        </table>
+    </section>
+    <section class="tabela">
+        <h2>WYMIANA OLEJU I FILTRÓW</h2>
+        <table>
+            <tr>
+                <th>Wymiana oleju silnikowego</th>
+                <td>Dobór i wymiana oleju oraz filtra oleju</td>
+                <th>100-200zł</th>
+            </tr>
+            <tr>
+                <th>Wymiana filtrów powietrza i kabinowego</th>
+                <td>Poprawa jakości powietrza w kabinie i silniku</td>
+                <th>50-100zł</th>
+            </tr>
+            <tr>
+                <th>Wymiana filtra paliwa</th>
+                <td>Wykrywanie nieszczelności, naprawa kompresora, wymiana osuszacza</td>
+                <th>80-200zł</th>
+            </tr>
+        </table>
+    </section>
+</body>
+</html>
