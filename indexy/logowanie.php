b6158a29bed887316094d6a831b376c407acf6eb
Hrysevich Viktoryia <Vik.hrysevich.work@gmail.com>
2025-03-26 15:44:21 +0200

    Update logowanie.php

--- indexy/logowanie.php
+++ indexy/logowanie.php
@@ -67,7 +67,7 @@
     <title>Logowanie</title>
 </head>
 <body>
-<p><a id="wracanie" href="">WRÓĆ</a></p>
+<p><a id="wracanie" href="główna.php">WRÓĆ</a></p>
 <br>
     <div class="logowanie">
         <h1>ZALOGUJ</h1>
