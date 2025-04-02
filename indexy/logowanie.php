23656bc4ed8cf890d16d7fa87228121e41ebbc2a
ascionia <ascionia@gmail.com>
2025-04-02 14:05:45 +0200

    Add files via upload

--- indexy/logowanie.php
+++ indexy/logowanie.php
@@ -1,83 +1,24 @@
+<?php require 'auth.php';?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Bricolage Grotesque' rel='stylesheet'>
-    <style>
-        *{
-            background-color: #2b1d1d;
-            color: white;
-            text-align: center;
-            font-family: 'Bricolage Grotesque';
-            #wracanie{
-                text-decoration: none;
-                float: left;
-                font-size: 20px;
-            }
-            #login{
-                background-color:#f0e9e2;
-                color: #909090;
-                border-color: #f0e9e2;
-                text-align: left;
-                height: 55px;
-                width: 500px;
-                font-size:35px;
-                text-decoration: none;
-                border-radius: 20px;
-                padding: 5px;
-
-            }
-            
-            #haslo{
-                background-color:#f0e9e2;
-                color: #909090;
-                border-color: #f0e9e2;
-                text-align: left;
-                height: 55px;
-                width: 500px;
-                font-size:35px;
-                text-decoration: none;
-                border-radius: 20px;
-                padding: 5px;
-                
-
-           
-            }
-            #submit{
-                background-color: #940000;
-                font-size:25px;
-                text-decoration: none;
-                border-radius: 20px;
-                padding: 15px;
-                padding-left: 210px;
-                padding-right: 210px;
-
-                
-               
-                
-            }
-            h1{
-                font-size: 75px;
-                
-            }
-        }
-        input{
-            border-style: none;
-        }
-    </style>
-    <title>Logowanie</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Logowanie się</title>
 </head>
 <body>
 <p><a id="wracanie" href="główna.php">WRÓĆ</a></p>
 <br>
-    <div class="logowanie">
-        <h1>ZALOGUJ</h1>
-        <input type="text" id="login" name="login" placeholder="Login"><br><br>
-        <input type="text" id="haslo"name="haslo" placeholder="Hasło"><br><br>
-        <input type="submit" id="submit" value="WYŚLIJ">
-        <p>LUB <a style="color:#780606"href="rejestracja.php">ZAREJESTRUJ SIĘ</a></p>
-    </div>
-    
+    <h1>ZALOGUJ SIĘ</h1>
+    <form method="post">
+        <input type="text" name="login" placeholder="Login*" required><br>
+        <input type="password" name="password"  placeholder="Hasło*" required><br>
+        <input type="hidden" name="action" value="login">
+        <input type="hidden" name="role" value="klient">
+        <button type="submit">WYŚLIJ</button>
+        <p>LUB <a style="color:#780606" href="rejestracja.php">ZAREJESTRUJ SIĘ</a></p>
+    </form>
 </body>
 </html>
